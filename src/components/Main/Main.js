88f1d84d4d2a0ef13c2571c69febec10c9aca373
Karpov Yuri <yurick@bk.ru>
2022-08-13 19:09:43 +0300

    feat: Сверстать секцию AboutMe

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import Promo from './components/Promo/Promo';
 import AboutProject from './components/AboutProject/AboutProject';
 import Techs from './components/Techs/Techs';
+import AboutMe from './components/AboutMe/AboutMe';
 
 import './Main.css';
 
@@ -10,6 +11,7 @@ function Main() {
       <Promo />
       <AboutProject />
       <Techs />
+      <AboutMe />
     </main>
   );
 };
---
+++ src/components/Main/components/AboutMe/AboutMe.js
@@ -0,0 +1,52 @@
+// import Portfolio from '../Portfolio/Portfolio';
+import './AboutMe.css';
+
+import myPhoto from '../../../../images/my-photo.jpg';
+
+function AboutMe() {
+  return (
+    <section className='about-me' id='about-me'>
+      <h1 className='title'>Студент</h1>
+      <hr className='line'></hr>
+      <div className='about-me__container'>
+        <div className='about-me__wrapper'>
+          <p className='about-me__name'>Юрий</p>
+          <p className='about-me__job'>Фронтенд-разработчик, 30 лет</p>
+          <p className='about-me__description'>
+            Я родился и живу в Воронеже, закончил факультет экономики ВГУИТ.
+            Люблю кататься на роликовых коньках и увлекаюсь автозвуком.
+            Год назад прошел отбор в "Яндекс.Практикум" на факультет "Веб-разработки"
+            по программе "Цифровая профессия в подарок".
+            С июня 2022 года прохожу стажировку в компании "Movika".
+          </p>
+          <ul className='about-me__social-links'>
+            <li>
+              <a
+                className='about-me__social-link'
+                target='_blank'
+                href='https://facebook.com'
+                rel="noreferrer"
+              >
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a
+                className='about-me__social-link'
+                target='_blank'
+                href='https://github.com/Yurick78'
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+        <img className='myPhoto' alt='Моя фотография' src={myPhoto} />
+      </div>
+      {/* <Portfolio /> */}
+    </section>
+  );
+};
+
+export default AboutMe;
