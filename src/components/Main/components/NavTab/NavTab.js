9944c6c2fd3e46fa35ba5e232f4d949de233f338
Karpov Yuri <yurick@bk.ru>
2022-08-14 01:57:33 +0300

    fix: Исправить верстку секции Promo

--- src/components/Main/components/NavTab/NavTab.js
+++ src/components/Main/components/NavTab/NavTab.js
@@ -3,14 +3,14 @@ import './NavTab.css';
 function NavTab() {
   return (
     <nav className='nav-tab'>
-      <ul className='nav-tab__list'>
-        <li className='nav-tab__elem'>
+      <ul className='nav-tab__links'>
+        <li className='nav-tab__link-wrapper hover'>
           <a className='nav-tab__link' href='#about-project'>О проекте</a>
         </li>
-        <li className='nav-tab__elem'>
+        <li className='nav-tab__link-wrapper hover'>
           <a className='nav-tab__link' href='#techs'>Технологии</a>
         </li>
-        <li className='nav-tab__elem'>
+        <li className='nav-tab__link-wrapper hover'>
           <a className='nav-tab__link' href='#about-me'>Студент</a>
         </li>
       </ul>
